a3cbd12625573ad298415e92c57d9b759822e8b2
georgesavin <31357590+georgesavin@users.noreply.github.com>
2017-09-12 11:52:27 -0600

    For Iterations
    coments are in the code

--- week-2/Program.cs
+++ week-2/Program.cs
@@ -6,75 +6,19 @@ namespace week2
     {
         public static void Main(string[] args)
         {
-            int x, y, a, b;
-
-            // Asignment operator
-            x = 3;
-            y = 2;
-            a = 1;
-            b = 0;
-
-            //There are many mathematical operations ....
-
-            //addition
-            x = 3 + 4;
-
-            //subtraction 
-            x = 4 - 3;
-
-            //multiplication
-            x = 10 * 5;
-
-            //division
-            x = 10 / 5;
-
-            // There are many operators used to evaluate values....
-
-            //equality
-            if (x == y)
+            static void Main(string[] args)
             {
+                for (int i = 0; i < 10; i++)
+                {
+                    //Console.WriteLine(i.ToString());
+                    if (i == 7)
+                }
+                Console.ReadLine("found 7");
+                break;
+           
             }
 
-			//greater than
-			if (x > y)
-			{
-			}
-
-			//less than
-			if (x < y)
-			{
-			}
-
-			//greater than or equal to
-			//greater than
-			if (x >= y)
-			{
-			}
-
-			//less than or equal to
-			//greater than
-			if (x <= y)
-			{
-			}
-
-			// There are two "conditional" operators as well that can be used to expand/ emhance an evaluation
-			// ... and they can be combined together multiple times.
-
-			//conditional AND operator
-            if ((x > y) && (a > b))
-			{
-			}
-
-			//conditional OR operator
-			if ((x > y) || (a > b))
-			{
-			}
-
-            // Also here is the inline conditional operator we learned about in the previous lesson
-            string message = (x == 1) ? "Car" : "Boat";
-
-            // Member acces and method invocation
-            Console.WriteLine("Hi");
+            
 		}
     }
 } 
